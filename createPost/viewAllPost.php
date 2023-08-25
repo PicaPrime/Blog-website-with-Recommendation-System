f0ec0ab508eab6dfa01849d9ad37df1634e4d63b
Md. Naimul Islam <113783674+PicaPrime@users.noreply.github.com>
2023-08-25 19:41:56 +0600

    triing something diffrent

--- createPost/viewAllPost.php
+++ createPost/viewAllPost.php
@@ -1,22 +1,22 @@
 <?php
-require_once('C:\xampp\htdocs\R_blog\dbConnection.php');
+require_once('C:/xampp/htdocs/R_blog/dbConnection.php');
 
 $sql = "SELECT *
     FROM content
     NATURAL JOIN content_tag
     NATURAL JOIN tag";
     
-$result = $conn->query($sql);
+$result = $connection->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo " - title: " . $row["title"]. "    ". $row["tag"]. "<br>";
+    echo '<a href="content.php?id=' . $row["content_id"] . '">' . $row["title"] . '</a><br>';
   }
 } else {
   echo "0 results";
 }
-$conn->close();
+$connection->close();
 
 
 ?>
