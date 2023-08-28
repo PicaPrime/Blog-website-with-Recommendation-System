276fc1809d673cf19873ee87d5d8c7b076ce9060
Md. Naimul Islam <113783674+PicaPrime@users.noreply.github.com>
2023-08-28 22:27:19 +0600

    restarting

--- login/content.php
+++ login/content.php
@@ -9,11 +9,10 @@
 
     <?php
     if (isset($_GET['id'])) {
-        // Database configuration
-        $host = "localhost"; // Change this to your database host
-        $username = "root"; // Change this to your database username
-        $password = ""; // Change this to your database password
-        $database = "blog"; // Change this to your database name
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "blog";
 
         // Create a connection
         $connection = new mysqli($host, $username, $password, $database);
@@ -32,56 +31,19 @@
             $row = $result->fetch_assoc();
             echo '<h1>' . $row["title"] . '</h1>';
             echo '<p>' . $row["body"] . '</p>';
-        } 
+        }
 
         if (isset($_COOKIE["user_id"])) {
             $stored_user_id = $_COOKIE["user_id"];
             echo $stored_user_id;
 
-            $sql = "select * from user_tag where user_id = '$stored_user_id' and tag_id = '$content_id';";
-            $result = $connection->query($sql);
-            while($row = $result->fetch_assoc()) {
-                echo "id: " . $row["user_id"]. " - Name: " . $row["tag_id"]. " " . $row["tag_priority"]. "<br>";
-              }
 
-            if ($result->num_rows == 0) {
-                $sql = "INSERT INTO `user_tag` (`user_id`, `tag_id`, `tag_priority`) VALUES ($stored_user_id, $content_id, 1);";
-                $result = $connection->query($sql);
-            }
-            else if ($result->num_rows > 0) {
-                // output data of each row
-                $newPriority = 0;
-                while($row = $result->fetch_assoc()) {
-                    $newPriority = $row["tag_priority"];
-                    $newPriority =$newPriority + 1;
-                }
-                
-                $sql = "INSERT INTO `user_tag` (`tag_priority`) VALUES ($newPriority) where user_id = '$stored_user_id' and tag_id = '$content_id';";
-                $result = $connection->query($sql);
-                echo "successfull";
-            } else {
-                echo "0 results";
-            }
+
+            // Close the connection
+            $connection->close();
         } else {
-            echo "User ID cookie not found.";
+            echo "Invalid request.";
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // Close the connection
-        $connection->close();
-    } else {
-        echo "Invalid request.";
     }
     ?>
 
