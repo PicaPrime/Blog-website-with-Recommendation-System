3f818154b38fff42e9ea4d5944adf51569571f85
Md. Naimul Islam <113783674+PicaPrime@users.noreply.github.com>
2023-09-01 22:47:03 +0600

    xamp delete

--- database.sql
+++ database.sql
@@ -3,7 +3,10 @@ CREATE TABLE `user` (
   `user_name` VARCHAR(50) NOT NULL,
   `user_password` VARCHAR(20) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
-  `dob` DATE
+  `dob` DATE,
+  `age` INT DEFAULT 0,
+  `ageRestriction` INT DEFAULT 1,
+  `country` VARCHAR(20)
 );
 
 CREATE TABLE `tag` (
@@ -23,7 +26,9 @@ CREATE TABLE `user_tag` (
 CREATE TABLE `admin` (
   `admin_id` INT PRIMARY KEY AUTO_INCREMENT,
   `admin_username` VARCHAR(20),
-  `admin_password` VARCHAR(20)
+  `admin_password` VARCHAR(20),
+  `admin_password` VARCHAR(20),
+  `admin_email` VARCHAR(100)
 );
 
 CREATE TABLE `content` (
@@ -40,6 +45,15 @@ CREATE TABLE `content_tag` (
   `tag_id` INT,
   PRIMARY KEY (`content_id`, `tag_id`)
 );
+
+CREATE TABLE `content_rating` (
+  `rating_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `content_id` INT,
+  `AverageRating` FLOAT(10),
+  `rating_date` TIMESTAMP,
+  FOREIGN KEY (`content_id`) REFERENCES `content` (`content_id`)
+);
+
 
 CREATE TABLE `comments` (
   `comment_id` INT PRIMARY KEY AUTO_INCREMENT,
@@ -62,50 +76,50 @@ ALTER TABLE `comments` ADD FOREIGN KEY (`content_id`) REFERENCES `content` (`con
 ALTER TABLE `comments` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 
+-- insert data
 
--- data insertion 
--- Inserting data into the 'user' table
-INSERT INTO `user` (`user_name`, `user_password`, `email`, `dob`)
-VALUES
-  ('john_doe', 'password123', 'john@example.com', '1990-05-15'),
-  ('jane_smith', 'securepass', 'jane@example.com', '1985-10-20'),
-  ('user3', 'mypassword', 'user3@example.com', '1998-02-28');
+INSERT INTO `user` (`user_name`, `user_password`, `email`, `dob`, `age`, `ageRestriction`, `country`) VALUES
+('JohnDoe', 'password123', 'john.doe@example.com', '1990-05-15', 31, 1, 'USA'),
+('AliceSmith', 'secret456', 'alice.smith@example.com', '1985-08-20', 36, 0, 'Canada'),
+('BobJohnson', 'p@ssw0rd', 'bob.johnson@example.com', '2000-02-10', 21, 1, 'UK');
 
--- Inserting data into the 'tag' table
-INSERT INTO `tag` (`name`)
-VALUES
-  ('Technology'),
-  ('Science'),
-  ('Art'),
-  ('Travel');
 
--- Inserting data into the 'user_tag' table
--- not inserting in user_tag
+INSERT INTO `tag` (`name`, `popularity_points`) VALUES
+('Technology', 100),
+('Travel', 75),
+('Food', 50);
 
--- Inserting data into the 'admin' table
-INSERT INTO `admin` (`admin_username`, `admin_password`)
-VALUES
-  ('admin1', 'adminpass123'),
-  ('admin2', 'adminsecure');
 
--- Inserting data into the 'content' table
-INSERT INTO `content` (`title`, `author`, `content_date`, `sub_title`, `body`)
-VALUES
-  ('Introduction to SQL', 'John Doe', '2023-08-01', 'Getting Started', 'This is a brief introduction to SQL...'),
-  ('The Science of Stars', 'Jane Smith', '2023-07-15', 'Stellar Phenomena', 'Stars are massive celestial objects...'),
-  ('Art Techniques', 'Artist123', '2023-08-10', 'Painting Basics', 'Learn the basics of mixing colors and brush techniques...');
+INSERT INTO `user_tag` (`user_id`, `tag_id`, `tag_priority`) VALUES
+(1, 1, 2),
+(1, 2, 1),
+(2, 2, 3),
+(3, 3, 1);
 
--- Inserting data into the 'content_tag' table
-INSERT INTO `content_tag` (`content_id`, `tag_id`)
-VALUES
-  (1, 1),
-  (2, 2),
-  (3, 3),
-  (3, 4);
 
--- Inserting data into the 'comments' table
-INSERT INTO `comments` (`content_id`, `user_id`, `comment_text`, `comment_date`)
-VALUES
-  (1, 1, 'Great article!', '2023-08-02 10:30:00'),
-  (1, 2, 'I found this very helpful.', '2023-08-02 12:45:00'),
-  (2, 3, 'I never knew stars had so much to offer.', '2023-07-15 14:20:00');
+INSERT INTO `admin` (`admin_username`, `admin_password`, `admin_email`) VALUES
+('admin1', 'adminpass1', 'admin1@example.com'),
+('admin2', 'adminpass2', 'admin2@example.com');
+
+INSERT INTO `content` (`title`, `author`, `content_date`, `sub_title`, `body`) VALUES
+('Introduction to SQL', 'John Doe', '2023-01-15', 'SQL Basics', 'This is an introductory article about SQL...'),
+('Travel Tips', 'Alice Smith', '2023-03-20', 'Exploring New Places', 'Learn how to make the most of your travel experience...'),
+('Delicious Recipes', 'Bob Johnson', '2023-02-10', 'Cooking Made Easy', 'Discover some mouthwatering recipes for food lovers...');
+
+INSERT INTO `content_tag` (`content_id`, `tag_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(3, 1);
+
+INSERT INTO `content_rating` (`content_id`, `AverageRating`, `rating_date`) VALUES
+(1, 4.5, '2023-01-16 09:30:00'),
+(2, 5.0, '2023-03-21 14:15:00'),
+(3, 4.2, '2023-02-11 18:45:00');
+
+INSERT INTO `comments` (`content_id`, `user_id`, `comment_text`, `comment_date`) VALUES
+(1, 1, 'Great article!', '2023-01-17 10:12:00'),
+(1, 2, 'I found it very helpful.', '2023-01-18 11:24:00'),
+(2, 3, 'Thanks for the tips!', '2023-03-22 15:30:00');
+
+
