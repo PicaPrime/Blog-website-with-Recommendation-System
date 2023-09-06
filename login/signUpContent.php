d8dfe8322cfdc4d89aab7445388111ca0700a992
Md. Naimul Islam <113783674+PicaPrime@users.noreply.github.com>
2023-09-06 14:27:55 +0600

    new

--- login/signUpContent.php
+++ login/signUpContent.php
@@ -4,8 +4,8 @@ $showAgeRestrictedContent = true;
 if (isset($_COOKIE["user_age"])) {
     $userAge = intval($_COOKIE["user_age"]);
     
-    if ($userAge < 18) {
-        $showAgeRestrictedContent = false; 
+    if ($userAge > 18) {
+        $showAgeRestrictedContent = true; 
     }
 }
 
@@ -32,12 +32,37 @@ if ($result->num_rows > 0) {
 
 $user_id = $_COOKIE["user_id"];
 
-$sql = "SELECT * FROM content natural join tag natural join user_tag where user_id = $user_id  ORDER BY tag_priority DESC LIMIT 10";
+if ($showAgeRestrictedContent) {
+    $sql = "SELECT * FROM content natural join content_tag natural join tag natural join user_tag where user_id = $user_id  ORDER BY tag_priority DESC LIMIT 5";
+} else {
+    $sql = "SELECT * FROM content natural join content_tag natural join tag natural join user_tag where user_id = $user_id and adult_tag = 0 ORDER BY tag_priority DESC LIMIT 5";
+
+}
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "<h1>Based on your Pervious Interaction</h1>";
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo '<a href="content.php?id=' . $row["content_id"] . '">' . $row["title"] . '</a><br>';
+    }
+    echo "</ul>";
+} else {
+    echo "No content titles found.";
+}
+
+
+if ($showAgeRestrictedContent) {
+    $sql = "SELECT * FROM content natural join tag ORDER BY popularity_points DESC LIMIT 5";
+} else {
+    $sql = "SELECT * FROM content natural join tag where adult_tag = 0 ORDER BY popularity_points DESC LIMIT 5";
+}
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<h1>Popular right now</h1>";
     echo "<ul>";
     while ($row = $result->fetch_assoc()) {
         echo '<a href="content.php?id=' . $row["content_id"] . '">' . $row["title"] . '</a><br>';
